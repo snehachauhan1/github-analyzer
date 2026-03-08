2a7c73e5fa1276e3dbe349c596764d210d75ca78
Sneha Chauhan <snehachauhancl1@gmail.com>
2026-03-08 17:13:17 +0530

    fix github username search bug

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, request
 import requests
 
 app = Flask(__name__)
@@ -8,68 +8,42 @@ def home():
     return render_template("index.html")
 
 
-@app.route("/api/<username>")
-def get_user(username):
+@app.route("/analyze", methods=["POST"])
+def analyze():
 
-    user_url = f"https://api.github.com/users/{username}"
-    repo_url = f"https://api.github.com/users/{username}/repos"
+    username = request.form["username"]
 
-    user_response = requests.get(user_url)
+    url = f"https://api.github.com/users/{username}/repos"
 
-    if user_response.status_code != 200:
-      return jsonify({"error": "User not found"})
-    
-    user_data = user_response.json()
-    repos = requests.get(repo_url).json()
+    response = requests.get(url)
 
-    total_stars = 0
-    languages = {}
+    if response.status_code == 404:
+        return "User not found"
 
-    top_repo = {"name": "", "stars": 0}
+    repos = response.json()
 
-    # loop through repositories
-    for repo in repos:
-
-        stars = repo["stargazers_count"]
-        total_stars += stars
-
-        # find top repo
-        if stars > top_repo["stars"]:
-            top_repo["name"] = repo["name"]
-            top_repo["stars"] = stars
-
-        # count languages
-        lang = repo["language"]
-        if lang:
-            languages[lang] = languages.get(lang, 0) + 1
-
-
-    # most used language
-    most_used_language = max(languages, key=languages.get) if languages else "None"
-
-
-    # get top 5 repositories by stars
+    # Top 5 repos by stars
     top_repos = sorted(
         repos,
         key=lambda repo: repo["stargazers_count"],
         reverse=True
     )[:5]
-    
-    developer_score = (
-    user_data.get("public_repos", 0) * 2 +
-    user_data.get("followers", 0) * 3 +
-    total_stars
-)
 
-    return jsonify({
-    "user": user_data,
-    "total_stars": total_stars,
-    "most_used_language": most_used_language,
-    "top_repo": top_repo,
-    "top_repos": top_repos,
-    "languages": languages
-})
+    # Language statistics
+    languages = {}
+
+    for repo in repos:
+        lang = repo["language"]
+        if lang:
+            languages[lang] = languages.get(lang, 0) + 1
+
+    return render_template(
+        "index.html",
+        repos=top_repos,
+        languages=languages,
+        username=username
+    )
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
